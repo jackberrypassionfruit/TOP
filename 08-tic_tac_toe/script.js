5f7b496a98cbda0d48caaa44d679dd8b3ca87170
jackberrypassionfruit <pashayanjack@gmail.com>
2022-07-29 12:52:53 -0400

    TOP proj 8 - added turns system to Tic Tac Toe

--- 08-tic_tac_toe/script.js
+++ 08-tic_tac_toe/script.js
@@ -1,28 +1,72 @@
 // Game board/displayController is a MODULE
 const gameBoard = (() => {
   // private methods
-  let board = [];
-  for (let i = 0; i < 3; i++) {
-    let row = [];
-    for (let j = 0; j < 3; j++) {
-      row.push("X");
-    }
-    board.push(row);
+  let board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+
+  function update(i, j, turn) {
+    board[i][j] = turn;
   }
   
-
   return { 
-    // return an object of the public methods
-    board
+    // public methods
+    board,
+    update
   };
 })();
 
-const displayController = ((board) => {
+const displayController = (() => {
   // private methods
+  function _turn(board) {
+    let x = 0;
+    let o = 0;
+    for (let i = 0; i<3; i++) {
+      for (let j = 0; j<3; j++) {
+        if (board[i][j] === "X") {
+          x++;
+        }
+        else if (board[i][j] === "O") {
+          o++;
+        }
+      }
+    }
+    if (x > o) {
+      return "O";
+    }
+    else {return "X"};
+  }
+  
+  function renderBoard(board) {
+    const gBoard = document.getElementsByClassName("gBoard")[0];
+
+    for (let i = 0; i < 3; i++) {
+      const row = document.createElement('div');
+      row.classList.add("row");
+
+      for (let j = 0; j < 3; j++) {
+        const box = document.createElement('div');
+        box.classList.add("box");
+        
+        const button = document.createElement('button');
+        button.classList.add("btn");
+
+        button.textContent = `${board[i][j]}`
+        button.addEventListener('click', (e) => {
+          if (button.textContent == " ") {
+            const turn = _turn(board);
+            button.textContent = turn;
+            gameBoard.update(i, j, turn);
+          }
+        })
+        box.appendChild(button);
+        row.appendChild(box);
+      }
+      gBoard.appendChild(row);
+    };
+  }
 
   return { 
     // return an object of the public methods
-
+    renderBoard
   };
 })();
 
@@ -35,4 +79,4 @@ const player = (name) => {
   return {name};
 }
 
-console.log(gameBoard.board);
+displayController.renderBoard(gameBoard.board);
