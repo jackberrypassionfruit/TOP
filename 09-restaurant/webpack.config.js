4ec8f300f4ac01d5f87c2bca2fe6a76f17fd9aff
jackberrypassionfruit <pashayanjack@gmail.com>
2022-08-01 00:48:57 -0400

    added TOP 09 restaurant barebones

--- 09-restaurant/webpack.config.js
+++ 09-restaurant/webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
 
 module.exports = {
- entry: {
-   index: './src/index.js',
-   print: './src/print.js',
- },
+mode: 'development',
+  entry: {
+    index: './src/script.js',
+  },
+  devtool: 'inline-source-map',
   output: {
-   filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        
+      },
+    ],
   },
 };
