f4efd4b313e1276f786a077e8cbd947383737b78
jackberrypassionfruit <pashayanjack@gmail.com>
2022-08-02 12:58:01 -0400

    TOP proj 10 todo - implemented visuals, needs responsiveness

--- 10-todo/src/script.js
+++ 10-todo/src/script.js
@@ -1,14 +1,103 @@
 import './style.css'
 
-localStorage.setItem('currentUser', 'jack');
 
-const userName = localStorage.getItem('currentUser');
-console.log(userName);
-document.getElementById('profile').innerHTML = userName;
-
-const testULs = document.getElementById('test').getElementsByTagName('li');
-for (let li of testULs) {
-  const btn = document.createElement('button');
-  btn.classList.add('done');
-  li.prepend(btn);
+class todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title,
+    this.description = description, 
+    this.dueDate = dueDate,
+    this.priority = priority};
 }
+
+
+class displayController  {
+  // Functions to handle rendering and DOM manipulation
+  constructor() {
+    this.newTodos = document.getElementById('newTodos');
+    this.categories = document.getElementsByClassName('categories')[0];
+    this.category = document.getElementById('category');
+  }
+
+  clearTodos() {
+    this.newTodos.innerHTML = '';
+  }
+
+  populateCategories() {
+    const lists = Object.keys(JSON.parse(localStorage.lists));
+
+    for (let key of lists) {
+      const btn = document.createElement('button');
+      btn.classList.add('btn');
+      btn.addEventListener('click', () => {
+        new displayController().populateTodos(key);
+      })
+      btn.innerHTML += key.toUpperCase();
+      this.categories.appendChild(btn);
+    }
+  }
+  
+  populateTodos(listType) {
+    this.clearTodos();
+
+    this.category.innerHTML = listType.toUpperCase();
+
+    // add Todos from localStorage to DOM
+    const todoObj = JSON.parse(localStorage.lists)[listType];
+
+    for (let key in todoObj) {
+      const newTodo = document.createElement('li');
+      const btn = document.createElement('button');
+      btn.classList.add('done');
+      newTodo.append(btn);
+      newTodo.innerHTML += `<span>${todoObj[key].description}</span>`;
+      newTodos.appendChild(newTodo);
+    };
+  };
+};
+
+class listModify  {
+
+  constructor() {
+    this.categories = ["todos", "reminders", "personal", "school"];
+  }
+
+  // Create
+  initialize() {
+    let ls = JSON.parse(window.localStorage.lists);
+
+    for (let key of this.categories) {
+      if (!(key in Object.keys(ls))) {ls[key] = {}}
+    }
+
+    localStorage.setItem("lists", JSON.stringify(ls));
+  }
+
+  examples() {
+    localStorage.setItem('currentUser', 'jack');
+    document.getElementById('profile').innerHTML = localStorage.getItem('currentUser');
+
+    let ls = JSON.parse(window.localStorage.lists);
+
+    const first = new todo("Nicole", "Write Love Letter to Nicole", "09/01", 1);
+    const second = new todo("bios", "Read Sophomore bios", "09/01", 2);
+    
+    let todos = {
+      first,
+      second,
+    };
+
+    ls["todos"] = todos;
+
+    localStorage.setItem("lists", JSON.stringify(ls));
+    // console.log(JSON.parse(window.localStorage.lists));
+
+  }
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  new displayController().populateCategories();
+  const lm = new listModify();
+  lm.initialize();
+  lm.examples();
+});
